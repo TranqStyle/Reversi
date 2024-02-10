118152112a01f7ee07d283d74323b2550a5b34ad
TranquiLiTy95 <68949655+TranquiLiTy95@users.noreply.github.com>
2024-02-10 23:23:36 +0300

    TranqPlayer: Add MultiRunner::announceResults() function and remove redundant code

--- Projects/Server/MultiGameRunner.cpp
+++ Projects/Server/MultiGameRunner.cpp
@@ -1,42 +1,33 @@
 #include "MultiGameRunner.h"
 
-MultiGameRunner::MultiGameRunner(const AbstractPlayer::Ptr& player0, const AbstractPlayer::Ptr& player1, const size_t& totalGamesToBePlayed = 1)
+MultiGameRunner::MultiGameRunner(const AbstractPlayer::Ptr& player0, const AbstractPlayer::Ptr& player1, size_t totalGamesToBePlayed = 1)
 	: _player0{ player0 }, _player1{ player1 }, _totalGamesToBePlayed{ totalGamesToBePlayed }
 {
-	_gameOutcomes[State::Outcome::P0WIN];
-	_gameOutcomes[State::Outcome::P1WIN];
-	_gameOutcomes[State::Outcome::DRAW];
-	_currentGameCount = 0;
 }
 
 
-void MultiGameRunner::play() 
+void MultiGameRunner::play()
 {
 	GameHandler gameHandler{};
 
-	for (size_t i{ 0 }; i < _totalGamesToBePlayed; ++i) 
-	{	
+	std::map<State::Outcome, size_t> gameOutcomes{ {State::Outcome::P0WIN, 0}, {State::Outcome::P1WIN, 0}, {State::Outcome::DRAW, 0} };
+
+	for (size_t i{ 0 }; i < _totalGamesToBePlayed; ++i)
+	{
 		gameHandler.init(_player0, _player1);
-		
 
 		State::Outcome outcome = gameHandler.play();
-		
-		++_currentGameCount;
-		++_gameOutcomes[outcome];
-		
-		std::cout << *this;
+
+		++gameOutcomes[outcome];
 	}
+
+	announceResults(gameOutcomes);
 }
 
-std::ostream& operator<<(std::ostream& os, const MultiGameRunner& multiGameRunner) 
+void MultiGameRunner::announceResults(const std::map<State::Outcome, size_t>& gameOutcomes) const 
 {	
-	os << "Games played: " << multiGameRunner._currentGameCount << " / " << multiGameRunner._totalGamesToBePlayed << "\n";
-	os << "P0( " << multiGameRunner._player0->getPreferedName() << " ) won: " << multiGameRunner._gameOutcomes.at(State::Outcome::P0WIN) << " game";
-	os << (multiGameRunner._gameOutcomes.at(State::Outcome::P0WIN) > 1 ? "s" : "") << "\n";
-	os << "P1( " << multiGameRunner._player1->getPreferedName() << " ) won: " << multiGameRunner._gameOutcomes.at(State::Outcome::P1WIN) << " game";
-	os << (multiGameRunner._gameOutcomes.at(State::Outcome::P1WIN) > 1 ? "s" : "") << "\n";
-	os << "Draws: " << multiGameRunner._gameOutcomes.at(State::Outcome::DRAW) << " game";
-	os << (multiGameRunner._gameOutcomes.at(State::Outcome::DRAW) > 1 ? "s" : "") << " drawn\n";
-	
-	return os;
+	std::cout << "Games Played: " << _totalGamesToBePlayed << "\n";
+	std::cout << "P0( " << _player0->getPreferedName() << " ) won: " << gameOutcomes.at(State::Outcome::P0WIN) << " game\n";
+	std::cout << "P1( " << _player1->getPreferedName() << " ) won: " << gameOutcomes.at(State::Outcome::P1WIN) << " game\n";
+	std::cout << "Draws: " << gameOutcomes.at(State::Outcome::DRAW) << " game\n";
 }	
--- Projects/Server/MultiGameRunner.h
+++ Projects/Server/MultiGameRunner.h
@@ -9,20 +9,17 @@
 class MultiGameRunner 
 {
 public:
-	MultiGameRunner(const AbstractPlayer::Ptr& player0, const AbstractPlayer::Ptr& player1, const size_t& gameSample);
+	MultiGameRunner(const AbstractPlayer::Ptr& player0, const AbstractPlayer::Ptr& player1, size_t totalGamesToBePlayed);
 	
-	friend std::ostream& operator<<(std::ostream& os, const MultiGameRunner& multiGameRunner);
-
 	void play();
+
+	void announceResults(const std::map<State::Outcome, size_t>&) const;
 	
 private:
 	AbstractPlayer::Ptr _player0;
 	AbstractPlayer::Ptr _player1;
 	
-	std::map<State::Outcome, size_t> _gameOutcomes;
-	
 	size_t _totalGamesToBePlayed;
-	size_t _currentGameCount;
 };
 
 #endif // MULTIGAMERUNNER_H
